f6dd4ebb20245a36d792fddf31f6650ab0cd62f5
loukastjean <2357611@cegepat.qc.ca>
2025-04-27 21:28:35 -0400

    Ajoute de la smoothness sur le mouvement de la camera

--- Assets/Scripts/GestionCamera.cs
+++ Assets/Scripts/GestionCamera.cs
@@ -6,10 +6,12 @@ public class Camera : MonoBehaviour
 {
     Camera cam;
     Personnage[] personnages = new Personnage[2];
+    float vitesseCam;
     
     // Start is called before the first frame update
     void Start()
     {
+        vitesseCam = 5f;
         cam = GetComponent<Camera>();
         personnages = FindObjectsOfType<Personnage>();
     }
@@ -17,8 +19,17 @@ public class Camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        BougerCamera();
+    }
+
+    void BougerCamera()
+    {
         Vector2 differencePersonnages = new(personnages[0].transform.position.x - personnages[1].transform.position.x, personnages[0].transform.position.y - personnages[1].transform.position.y);
         differencePersonnages /= 2;
-        transform.position = new(differencePersonnages.x + personnages[1].transform.position.x, differencePersonnages.y + personnages[1].transform.position.y, transform.position.z);
+        Vector3 destination = new(differencePersonnages.x + personnages[1].transform.position.x,
+            differencePersonnages.y + personnages[1].transform.position.y);
+        Vector3 mouvementCamera = new(destination.x - transform.position.x, destination.y - transform.position.y);
+        
+        transform.position += mouvementCamera * vitesseCam * Time.deltaTime;
     }
 }
